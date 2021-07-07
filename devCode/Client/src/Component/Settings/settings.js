1b1ef61ad04204209f7a8581214583bfe5245d6f
rahibarot <rahibarot1997@gmail.com>
2021-07-07 16:04:49 -0400

    settings

---
+++ devCode/Client/src/Component/Settings/settings.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './settings.css';
+
+//Setting should have following
+// Audio: Input, Output, Microphone: mute , Volume: 70
+//Peripherals. ConectedDevice.Status & Name' to turn on and off or switch path manually
+// LocalInput: 
+class Settings extends Component{
+    render() {
+        return(
+            "hello"
+        );
+    }
+}
